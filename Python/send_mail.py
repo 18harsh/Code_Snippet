9a34be629c0bad474a22091100ece8ef3b1ff23c
Harsh Gandhi <18harshgandhi@gmail.com>
2020-04-27 15:16:13 +0530

    Update send_mail.py

--- Python/send_mail.py
+++ Python/send_mail.py
@@ -11,6 +11,12 @@ def send_email(subject,msg):
     email = input("enter sender's email")
     try:
         server = smtplib.SMTP("smtp.gmail.com:587")
+        #Gmail smtp.gmail.com
+        # Outlook.com/Hotmail.com smtp-mail.outlook.com
+        # Yahoo Mail smtp.mail.yahoo.com
+        # AT&T smpt.mail.att.net (port 465)
+        # Comcast smtp.comcast.net
+        # Verizon smtp.verizon.net (port 465)
         server.ehlo()#start connection
         server.starttls()
         server.login(config.email_address,config.password)
